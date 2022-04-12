e4ab1830ca60ce6c2a83c797cdf123849fb089f9
ashwin <ashwinkolkar22@gmail.com>
2022-04-12 14:29:33 +0530

    Count Character By Hash Map Without Usgin Array Not Issue With Capital Or Samll

--- src/com/logical/CountCharByHashMap.java
+++ src/com/logical/CountCharByHashMap.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 public class CountCharByHashMap {
 	public static void main(String[] args) {
 
-		String input = "india";
+		String input = "India";
 
 		HashMap<Character, Integer> hashMap = new HashMap<>();
 
 		for (int i = 0; i < input.length(); i++) {
 			char ch = input.charAt(i);
+			int word = (int) ch;
+
+			// If Occured Character is Capital Letter then Convert it to Small
+
+			if (word < 97) {
+				ch = (char) (word + 32);
+			}
 			if (hashMap.containsKey(ch)) {
 				int val = hashMap.get(ch);
 				hashMap.put(ch, val + 1);
@@ -18,6 +25,6 @@ public class CountCharByHashMap {
 				hashMap.put(ch, 1);
 			}
 		}
-		System.out.println(hashMap);
+		System.out.println("Word Count: "+hashMap);
 	}
 }
